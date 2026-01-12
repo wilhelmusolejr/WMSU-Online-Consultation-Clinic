a5a3a0fd96a65006a29a5bef2030629012ad9372
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2026-01-12 20:22:12 +0800

    style: remove 10/12 width

--- client/src/components/FeatureCard.jsx
+++ client/src/components/FeatureCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FeatureCard() {
   return (
-    <div className="bg-offwhite text-primary rounded-xl p-2 border border-black/20 w-10/12 mx-auto">
+    <div className="bg-offwhite text-primary rounded-xl p-2 border border-black/20">
       <div className="bg-white min-h-56 rounded-xl border border-black/20 py-10">
         {/* image */}
         <div className="w-30 h-30 rounded-lg border bg-offwhite border-black/20 mx-auto  "></div>
