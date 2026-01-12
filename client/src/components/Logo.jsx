3de306176bb1a687a6080e6ff378c4b02209309a
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2026-01-13 00:37:29 +0800

    feat: add logo for navigator

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import logoImg from "../assets/logo.png";
+
 export default function Logo() {
   return (
     <div className="flex items-center gap-3">
-      <div className="h-12 w-12 rounded-full border border-black/50"></div>
-      <p className="font-bold uppercase text-[#0B5534]">Home economics</p>
+      <div className="h-16 w-16 rounded-full border border-black/50">
+        <img src={logoImg} alt="" />
+      </div>
+      <p className="font-bold text-xl uppercase text-[#0B5534]">
+        Home economics
+      </p>
     </div>
   );
 }
